7ab3fca9ca6aa1a25f14a66c4fb747b4e3e2616e
The-Snide-Sniper <5t3v0dud3@gmail.com>
2018-01-11 20:20:20 -0600

    Debugging 1/11/17
    I fixed a few careless mistakes.

--- gen.php
+++ gen.php
@@ -9,18 +9,19 @@
         <th>B</th><th>I</th><th>N</th><th>G</th><th>O</th>
       </tr>
       <tr>
-        <?php
+        <td>
+          <?php
         $possible_tiles = file("./boards/" . $_GET['board'] . ".txt");
         $tiles = array();
         for ($i = 0; $i < 5; $i++) {
           $row = array();
           for ($j = 0; $j < 5; $j++) {
-            array_push($row, array_splice($possible_tiles, rand(0, count($possible_tiles) - 1))[0]);
+            array_push($row, array_splice($possible_tiles, rand(0, count($possible_tiles) - 1))[0], 1);
           }
-          array_push(implode($row, "<\/td><td>"));
+          array_push($tiles, implode($row, "        </td>\n        <td>\n          "));
         }
-        echo implode($tiles, "<\/td><\/tr><tr><td>");
-        ?>
+        echo implode($tiles, "        </td>\n      </tr>\n      <tr>\n        <td>\n          ");
+        ?>        </td>
       </tr>
     </table>
   </body>
