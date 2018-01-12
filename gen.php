a4f469d0b78db68c36f30b60dd04c8b982feff24
The-Snide-Sniper <5t3v0dud3@gmail.com>
2018-01-11 18:33:13 -0600

    Update gen.php

--- gen.php
+++ gen.php
@@ -10,7 +10,16 @@
       </tr>
       <tr>
         <?php
-        $tiles = file("./boards/" . $_GET['board'] . ".txt");
+        $possible_tiles = file("./boards/" . $_GET['board'] . ".txt");
+        $tiles = array();
+        for ($i = 0; $i < 5; $i++) {
+          $row = array();
+          for ($j = 0; $j < 5; $j++) {
+            array_push($row, array_splice($possible_tiles, rand(0, count($possible_tiles) - 1))[0]);
+          }
+          array_push(implode($row, "<\/td><td>"));
+        }
+        echo implode($tiles, "<\/td><\/tr><tr><td>");
         ?>
       </tr>
     </table>
